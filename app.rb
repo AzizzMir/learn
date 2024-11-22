77e1e30157c1b643429c5bccbe508c2c0a7c361b
Handamier Mieraizizi <ezizmir9@gmail.com>
2024-11-22 19:18:25 +0000

    made index and seperrate page for every letter

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @letters = ["ئا", "ئە", "ب", "پ", "ت", "ج", "چ", "خ", "د", "ر", "ز", "ژ", "س", "ش", "غ", "ف", "ج", "ك", "گ", "ڭ", "ل", "م", "ن", "ھ", "ئو", "ئۇ", "ئ‍ۆ", "ئ‍ۈ", "ۋ", "ئى", "ئې", "ي"]
+
+  erb(:index)
+end
+
+get("/:letter") do
+  @chosed_letter = params.fetch("letter")
+
+  erb(:flexible_letter)
 end
